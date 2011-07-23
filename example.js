91a06564b235b46cab6d7c6c4bffaa2c126f97d7
Isofarro <isofarro2@gmail.com>
2011-07-23 17:32:55 +0100

    failed to get a setInterval working inside the Pulse.

--- example.js
+++ example.js
@@ -2,9 +2,12 @@ var pulse   = require('./lib/pulse'),
 	myPulse = new pulse.Pulse(),
 	ticks   = 0;
 
-myPulse.start()
+myPulse
+	//.config({ tickInterval: 1000, maxTicks: 10 })
+	.config({ tickInterval: 1000 })
 	.on('tick', function() { ticks++; console.log('tick-tock!'); })
-	.on('end',  function() { console.log('Ticks: ' + ticks); });
+	.on('end',  function() { console.log('Ticks: ' + ticks); })
+	.start();
 
 
 setTimeout(function() {
