b05a02b3eca9d17009b54565faa013bbaf0502b1
Isofarro <isofarro2@gmail.com>
2011-07-24 19:54:59 +0100

    Clean up example code, prior to introducing the start event

--- example.js
+++ example.js
@@ -1,27 +1,10 @@
-var //pulse     = require('./lib/pulse'),
-	waterflow = require('./models/waterflow'),
-	myModel   = new waterflow.WaterFlow(),
-	//myPulse   = new pulse.Pulse({}),
-	ticks     = 0;
+var waterflow = require('./models/waterflow'),
+	myModel   = new waterflow.WaterFlow();
 
-// TODO: Refactor to get myModel to use myPulse
-/****
-myPulse
-	.config({ tickInterval: 1000, maxTicks: 10 })
-	.on('end',  function() { console.log('Ticks: ' + myPulse.ticks); })
-	.on('tick', function() { myModel.tick() })
-	.on('end',  function() { console.log('Simulation completed'); })
-	.start();
-****/
-
-
-// /********
-// Preferred interface
 myModel
 	.config({ maxTicks: 10, tickInterval: 1000 })
-	.on('tick', function() { console.log(myModel.status());         })
+	.on('tick', function() { console.log( myModel.status() );     })
 	.on('end',  function() { console.log('Simulation completed'); })
 	.start();
-// ********/
 
 
